7c7a1ebe68c00b0b2688f9aa32e2f0de0b189303
Terry Lippincott <tlippincott25@gmail.com>
2022-04-16 11:12:01 -1000

    basic functioning app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            monsters: [],
+            searchField: '',
+        };
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json())
+            .then((users) =>
+                this.setState(() => {
+                    return { monsters: users };
+                })
+            );
+    }
+
+    onSearchChange = (event) => {
+        const searchField = event.target.value.toLowerCase();
+
+        this.setState(() => {
+            return { searchField };
+        });
+    };
+
+    render() {
+        const { monsters, searchField } = this.state;
+        const { onSearchChange } = this;
+
+        const filteredMonsters = monsters.filter((monster) => {
+            return monster.name.toLowerCase().includes(searchField);
+        });
+
+        return (
+            <div className='App'>
+                <input
+                    className='search-box'
+                    type='search'
+                    placeholder='search monsters'
+                    onChange={onSearchChange}
+                />
+                {filteredMonsters.map((monster) => {
+                    return <h1 key={monster.id}>{monster.name}</h1>;
+                })}
+            </div>
+        );
+    }
 }
 
 export default App;
