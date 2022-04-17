1225e54bc691eecdad53acf73c90a82ab44687fb
Terry Lippincott <tlippincott25@gmail.com>
2022-04-17 13:59:14 -1000

    Added CSS file for SearchBox component and for the background

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ class App extends Component {
                 <SearchBox
                     onChangeHandler={onSearchChange}
                     placeholder='search monsters'
-                    className='search-box'
+                    className='monsters-search-box'
                 />
                 <CardList monsters={filteredMonsters} />
             </div>
